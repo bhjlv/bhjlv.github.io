75b0f02e92fab95a90586fd552e500806d190618
Vitor Bigelli <vitorbigelli@hotmail.com>
2020-09-01 15:06:11 -0300

    Add icons to methodology section

--- src/views/examples/landing-page-sections/Metodologia.js
+++ src/views/examples/landing-page-sections/Metodologia.js
@@ -8,8 +8,15 @@ import {
 
 //assets
 import "../../../assets/css/custom.css"
-import laptop from '../../../assets/img/laptop.png'
+import laptop from '../../../assets/img/laptop.png' 
 
+
+import brainstorm_svg from '../../../assets/svg/idea.svg'
+import communication_svg from '../../../assets/svg/communication.svg'
+import dev_svg from '../../../assets/svg/rocket.svg'
+import documentation_svg from '../../../assets/svg/browser.svg'
+import validation_svg from '../../../assets/svg/finger.svg'
+import requirements_svg from '../../../assets/svg/wrench.svg'
 
 function Metodologia () {
     return (
@@ -29,21 +36,21 @@ function Metodologia () {
                 <Container className="howework">
                 <Row>
                     <Col className="workitem">
-                    <img src={laptop} className="workimg" alt=""></img>
+                    <img src={requirements_svg} className="workimg" alt=""></img>
                     <h5 className="worktitle">Estudo dos requisitos</h5>
-                    <h6 className="workdescription">
+                    <h6 className="workdescription"> 
                         Suspendisse vitae velit erat. Quisque volutpat libero id leo convallis, sed consequat nunc scelerisque. Ut.
                     </h6>
                     </Col>
                     <Col className="workitem">
-                    <img src={laptop} className="workimg" alt=""></img>
+                    <img src={brainstorm_svg} className="workimg" alt=""></img>
                     <h5 className="worktitle">Brainstorm</h5>
                     <h6 className="workdescription">
                         Suspendisse vitae velit erat. Quisque volutpat libero id leo convallis, sed consequat nunc scelerisque. Ut.
                     </h6>
                     </Col>
                     <Col className="workitem">
-                    <img src={laptop} className="workimg" alt=""></img>
+                    <img src={validation_svg} className="workimg" alt=""></img>
                     <h5 className="worktitle">Validação das ideias</h5>
                     <h6 className="workdescription">
                         Suspendisse vitae velit erat. Quisque volutpat libero id leo convallis, sed consequat nunc scelerisque. Ut.
@@ -52,21 +59,21 @@ function Metodologia () {
                 </Row>
                 <Row>
                     <Col className="workitem">
-                    <img src={laptop} className="workimg" alt=""></img>
+                    <img src={dev_svg} className="workimg" alt=""></img>
                     <h5 className="worktitle">Desenvolvimento</h5>
                     <h6 className="workdescription">
                         Suspendisse vitae velit erat. Quisque volutpat libero id leo convallis, sed consequat nunc scelerisque. Ut.
                     </h6>
                     </Col>
                     <Col className="workitem">
-                    <img src={laptop} className="workimg" alt=""></img>
+                    <img src={communication_svg} className="workimg" alt=""></img>
                     <h5 className="worktitle">Comunicação e entregas</h5>
                     <h6 className="workdescription">
                         Suspendisse vitae velit erat. Quisque volutpat libero id leo convallis, sed consequat nunc scelerisque. Ut.
                     </h6>
                     </Col>
                     <Col className="workitem">
-                    <img src={laptop} className="workimg" alt=""></img>
+                    <img src={documentation_svg} className="workimg" alt=""></img>
                     <h5 className="worktitle">Documentação</h5>
                     <h6 className="workdescription">
                         Suspendisse vitae velit erat. Quisque volutpat libero id leo convallis, sed consequat nunc scelerisque. Ut.
