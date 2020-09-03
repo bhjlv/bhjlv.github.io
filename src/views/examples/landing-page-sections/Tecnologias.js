105c60d313d6dd08174c239a94f39a96deaeeb21
Vitor Bigelli <vitorbigelli@hotmail.com>
2020-09-03 20:00:24 -0300

    Add icons to technologies section

--- src/views/examples/landing-page-sections/Tecnologias.js
+++ src/views/examples/landing-page-sections/Tecnologias.js
@@ -8,6 +8,32 @@ import {
 
 //style
 import "../../../assets/css/custom.css"
+
+
+import {
+    DiNodejsSmall,
+    DiReact, 
+    DiJavascript, 
+    DiPython,
+    DiMongodb,
+    DiHtml5,
+    DiCss3,
+} from 'react-icons/di'
+import {
+    SiKubernetes,
+    SiRabbitmq,
+    SiNeo4J,
+    SiTensorflow,
+    SiPytorch,
+    SiPostgresql,
+    SiFlask,
+    SiDocker,
+    SiNodeDotJs,
+    SiReact
+} from 'react-icons/si'
+
+
+
 import undraw_code_svg from '../../../assets/svg/undraw_code.svg'
 
 function Tecnologias () {
@@ -27,31 +53,83 @@ function Tecnologias () {
                 <Container className="technologies">
                     <Row>
                         <Col xs={12} md={6}> 
-                            <img src={undraw_code_svg}  />
+                            <img src={undraw_code_svg} className='technologies-image'  />
                         </Col>
-                        <Col xs={12} sm={4} md={4}>
-                        <ul className="technologies-list">
-                            <li>Python</li>
-                            <li>JavaScript</li>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                        </ul>
-                        </Col>
-                        <Col xs={12} sm={4}  md={4}>
-                        <ul className="technologies-list">
-                            <li>React</li>
-                            <li>React Native</li>
-                            <li>NodeJS</li>
-                            <li>Neo4j</li>
-                        </ul>
-                        </Col>
-                        <Col xs={12} sm={4} md={4}>
-                        <ul className="technologies-list">
-                            <li>MongoDB</li>
-                            <li>Docker</li>
-                            <li>Kubernetes</li>
-                            <li>Tensorflow/PyTorch</li>
-                        </ul>
+                        <Col xs={12} md={6}>
+                            <Container className='technologies-list'>
+                                <Row>
+                                    <Col>
+                                        <DiPython />
+                                        <p> Python </p>
+                                    </Col>
+                                    <Col>
+                                        <DiJavascript/>
+                                        <p> JavaScript </p>
+                                    </Col>
+                                    <Col>
+                                        <DiHtml5/>
+                                        <p>HTML</p>
+                                    </Col>
+                                    <Col> 
+                                        <DiCss3 />                               
+                                        <p>CSS</p>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>   
+                                        <SiReact />
+                                        <p>React</p>
+                                    </Col>
+                                    <Col>
+                                        <SiReact />
+                                        <p>React Native</p>
+                                    </Col>
+                                    <Col>
+                                        <SiNodeDotJs />
+                                        <p>NodeJS</p>
+                                    </Col>
+                                    <Col>  
+                                        <SiFlask />          
+                                        <p>Flask</p>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <DiMongodb />
+                                        <p>MongoDB</p>
+                                    </Col>
+                                    <Col>
+                                        <SiNeo4J />          
+                                        <p>Neo4j</p>
+                                    </Col>
+                                    <Col>
+                                        <SiPostgresql />
+                                        <p>PostgressQL</p>
+                                    </Col>
+                                    <Col>      
+                                        <SiRabbitmq />      
+                                        <p>RabbitMQ</p>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <SiDocker />
+                                        <p>Docker</p>
+                                    </Col>
+                                    <Col>
+                                        <SiKubernetes />
+                                        <p>Kubernetes</p>
+                                    </Col>
+                                    <Col>
+                                        <SiTensorflow />      
+                                        <p>Tensorflow</p>
+                                    </Col>
+                                    <Col>      
+                                        <SiPytorch />      
+                                        <p> PyTorch</p>
+                                    </Col>
+                                </Row>
+                            </Container>
                         </Col>
                     </Row>
                 </Container>
