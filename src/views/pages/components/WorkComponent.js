792f6d3102611ab83fe8f8ac9e2b4754970a9c83
Vitor <bigellivitor@gmail.com>
2020-09-09 19:08:44 -0300

    Add react-reveal fade effect to methodologies page

--- src/views/pages/components/WorkComponent.js
+++ src/views/pages/components/WorkComponent.js
@@ -8,7 +8,12 @@ import undraw_brainstorm from '../../../assets/svg/undraw_brainstorm.svg'
 import undraw_idea from '../../../assets/svg/undraw_idea.svg'
 import undraw_programming from '../../../assets/svg/undraw_programming.svg'
 import undraw_delivery from '../../../assets/svg/undraw_delivery.svg'
-import undraw_documentation from '../../../assets/svg/undraw_documentation.svg'
+import undraw_documentation from '../../../assets/svg/undraw_documentation.svg' 
+
+
+
+import Fade from 'react-reveal/Fade'
+
 
 function WorkComponent() {
 
@@ -21,72 +26,84 @@ function WorkComponent() {
                     <h1> Como trabalhamos</h1>
                 </Col>
             </Row>
-            <Row className='metodologia-item'>
-                <Col>
-                    <img src={undraw_requirements} />
-                </Col>
-                <Col className='text-left'>
-                    <h4> Estudo de requisitos</h4>
-                    <p>
-                        Suspendisse a nulla gravida, congue metus at, aliquam augue. Aenean congue vulputate egestas. Donec vulputate dui id sapien aliquet congue. Quisque quis erat vitae elit porttitor fringilla id sed nibh. Suspendisse eget ligula bibendum, aliquam quam in, efficitur lectus. Praesent lobortis, ante at suscipit sagittis, neque sapien pulvinar odio, sed.
-                    </p>
-                </Col>
-            </Row>
-            <Row className='metodologia-item'>
-                <Col className='text-right'>
-                    <h4> Brainstorming </h4>
-                    <p>
-                        Suspendisse a nulla gravida, congue metus at, aliquam augue. Aenean congue vulputate egestas. Donec vulputate dui id sapien aliquet congue. Quisque quis erat vitae elit porttitor fringilla id sed nibh. Suspendisse eget ligula bibendum, aliquam quam in, efficitur lectus. Praesent lobortis, ante at suscipit sagittis, neque sapien pulvinar odio, sed.
-                    </p>
-                </Col>
-                <Col>
-                    <img src={undraw_brainstorm} />
-                </Col>
-            </Row>
-            <Row className='metodologia-item'>
-                <Col>
-                    <img src={undraw_idea} />
-                </Col>
-                <Col className='text-left'>
-                    <h4> Validação de Ideias </h4>
-                    <p>
-                        Suspendisse a nulla gravida, congue metus at, aliquam augue. Aenean congue vulputate egestas. Donec vulputate dui id sapien aliquet congue. Quisque quis erat vitae elit porttitor fringilla id sed nibh. Suspendisse eget ligula bibendum, aliquam quam in, efficitur lectus. Praesent lobortis, ante at suscipit sagittis, neque sapien pulvinar odio, sed.
-                    </p>
-                </Col>
-            </Row>
-            <Row className='metodologia-item'>
-                <Col className='text-right'>
-                    <h4> Desenvolvimento </h4>
-                    <p>
-                        Suspendisse a nulla gravida, congue metus at, aliquam augue. Aenean congue vulputate egestas. Donec vulputate dui id sapien aliquet congue. Quisque quis erat vitae elit porttitor fringilla id sed nibh. Suspendisse eget ligula bibendum, aliquam quam in, efficitur lectus. Praesent lobortis, ante at suscipit sagittis, neque sapien pulvinar odio, sed.
-                    </p>
-                </Col>
-                <Col>
-                    <img src={undraw_programming} />
-                </Col>
-            </Row>
-            <Row className='metodologia-item'>
-                <Col>
-                    <img src={undraw_delivery} />
-                </Col>
-                <Col className='text-left'>
-                    <h4>Comunicação e Entregas </h4>
-                    <p>
-                        Suspendisse a nulla gravida, congue metus at, aliquam augue. Aenean congue vulputate egestas. Donec vulputate dui id sapien aliquet congue. Quisque quis erat vitae elit porttitor fringilla id sed nibh. Suspendisse eget ligula bibendum, aliquam quam in, efficitur lectus. Praesent lobortis, ante at suscipit sagittis, neque sapien pulvinar odio, sed.
-                    </p>
-                </Col>
-            </Row>
-            <Row className='metodologia-item'>
-                <Col className='text-right'>
-                    <h4> Documentação </h4>
-                    <p>
-                        Suspendisse a nulla gravida, congue metus at, aliquam augue. Aenean congue vulputate egestas. Donec vulputate dui id sapien aliquet congue. Quisque quis erat vitae elit porttitor fringilla id sed nibh. Suspendisse eget ligula bibendum, aliquam quam in, efficitur lectus. Praesent lobortis, ante at suscipit sagittis, neque sapien pulvinar odio, sed.
-                    </p>
-                </Col>
-                <Col>
-                    <img src={undraw_documentation} />
-                </Col>
-            </Row>
+            <Fade left>
+                <Row className='metodologia-item'> 
+                        <Col>
+                            <img src={undraw_requirements} />
+                        </Col>
+                        <Col className='text-left'>
+                            <h4> Estudo de requisitos</h4>
+                            <p>
+                                Suspendisse a nulla gravida, congue metus at, aliquam augue. Aenean congue vulputate egestas. Donec vulputate dui id sapien aliquet congue. Quisque quis erat vitae elit porttitor fringilla id sed nibh. Suspendisse eget ligula bibendum, aliquam quam in, efficitur lectus. Praesent lobortis, ante at suscipit sagittis, neque sapien pulvinar odio, sed.
+                            </p>
+                        </Col>
+                </Row>
+            </Fade>
+            <Fade right>
+                <Row className='metodologia-item'>
+                    <Col className='text-right'>
+                        <h4> Brainstorming </h4>
+                        <p>
+                            Suspendisse a nulla gravida, congue metus at, aliquam augue. Aenean congue vulputate egestas. Donec vulputate dui id sapien aliquet congue. Quisque quis erat vitae elit porttitor fringilla id sed nibh. Suspendisse eget ligula bibendum, aliquam quam in, efficitur lectus. Praesent lobortis, ante at suscipit sagittis, neque sapien pulvinar odio, sed.
+                        </p>
+                    </Col>
+                    <Col>
+                        <img src={undraw_brainstorm} />
+                    </Col>
+                </Row>
+            </Fade>
+            <Fade left>
+                <Row className='metodologia-item'>
+                    <Col>
+                        <img src={undraw_idea} />
+                    </Col>
+                    <Col className='text-left'>
+                        <h4> Validação de Ideias </h4>
+                        <p>
+                            Suspendisse a nulla gravida, congue metus at, aliquam augue. Aenean congue vulputate egestas. Donec vulputate dui id sapien aliquet congue. Quisque quis erat vitae elit porttitor fringilla id sed nibh. Suspendisse eget ligula bibendum, aliquam quam in, efficitur lectus. Praesent lobortis, ante at suscipit sagittis, neque sapien pulvinar odio, sed.
+                        </p>
+                    </Col>
+                </Row>
+            </Fade>
+            <Fade right>
+                <Row className='metodologia-item'>
+                    <Col className='text-right'>
+                        <h4> Desenvolvimento </h4>
+                        <p>
+                            Suspendisse a nulla gravida, congue metus at, aliquam augue. Aenean congue vulputate egestas. Donec vulputate dui id sapien aliquet congue. Quisque quis erat vitae elit porttitor fringilla id sed nibh. Suspendisse eget ligula bibendum, aliquam quam in, efficitur lectus. Praesent lobortis, ante at suscipit sagittis, neque sapien pulvinar odio, sed.
+                        </p>
+                    </Col>
+                    <Col>
+                        <img src={undraw_programming} />
+                    </Col>
+                </Row>
+            </Fade>
+            <Fade left>
+                <Row className='metodologia-item'>
+                    <Col>
+                        <img src={undraw_delivery} />
+                    </Col>
+                    <Col className='text-left'>
+                        <h4>Comunicação e Entregas </h4>
+                        <p>
+                            Suspendisse a nulla gravida, congue metus at, aliquam augue. Aenean congue vulputate egestas. Donec vulputate dui id sapien aliquet congue. Quisque quis erat vitae elit porttitor fringilla id sed nibh. Suspendisse eget ligula bibendum, aliquam quam in, efficitur lectus. Praesent lobortis, ante at suscipit sagittis, neque sapien pulvinar odio, sed.
+                        </p>
+                    </Col>
+                </Row>
+            </Fade>
+            <Fade right>
+                <Row className='metodologia-item'>
+                    <Col className='text-right'>
+                        <h4> Documentação </h4>
+                        <p>
+                            Suspendisse a nulla gravida, congue metus at, aliquam augue. Aenean congue vulputate egestas. Donec vulputate dui id sapien aliquet congue. Quisque quis erat vitae elit porttitor fringilla id sed nibh. Suspendisse eget ligula bibendum, aliquam quam in, efficitur lectus. Praesent lobortis, ante at suscipit sagittis, neque sapien pulvinar odio, sed.
+                        </p>
+                    </Col>
+                    <Col>
+                        <img src={undraw_documentation} />
+                    </Col>
+                </Row>
+            </Fade>
         </Container>
         
         </>
